9b49ffcbf163a0b4dc40ff35a1ca8cbf59987eec
Jan Laureys <janl@hakka.eu>
2014-10-30 14:35:27 +0100

    Added show more - show less tags button

--- htdocs/profiles/howest_newsletter/themes/howest_newsletter/js/script.js
+++ htdocs/profiles/howest_newsletter/themes/howest_newsletter/js/script.js
@@ -7,6 +7,31 @@ jQuery.noConflict();
       $(this).append($(this).closest('.node').find('.field-name-field-location'));
       $(this).append($(this).closest('.node').find('.field-name-field-content'));
     });
+
+    if($('#block-views-tag-cloud-block .views-row').length > 10){
+      $('#block-views-tag-cloud-block .views-row').slice(10).hide();
+      $('#block-views-tag-cloud-block').append('<a class="show-all-tags" href="#">Toon meer tags</a>');
+
+      var visible = false;
+
+      $('.show-all-tags').click(function(e){
+        e.preventDefault();
+
+        if(!visible){
+          $('#block-views-tag-cloud-block .views-row').slideDown('fast');
+          $(this).text('Toon minder tags');
+          visible = true;
+        } else {
+          $('#block-views-tag-cloud-block .views-row').slice(10).slideUp('fast');
+          $(this).text('Toon meer tags');
+          visible = false;
+        }
+      });
+    }
+
+
+
   });
+
 
 }(jQuery));
