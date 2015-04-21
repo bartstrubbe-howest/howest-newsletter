39d6d72a0b3eb2486545de180ec058ee65b3c751
Wouter Haesaerts <wouter@inuits.eu>
2015-04-21 12:47:26 +0200

    updated theme, added views_fieldsets module.

--- htdocs/profiles/howest_newsletter/themes/howest_newsletter/templates/page.tpl.php
+++ htdocs/profiles/howest_newsletter/themes/howest_newsletter/templates/page.tpl.php
@@ -116,9 +116,10 @@
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
+        <a href="<?php print $front_page; ?>" title="Inside Howest" id="brandingLogo">inside <strong>howest</strong></a>
+        <i class="triangle"></i>
+
+
       <?php endif; ?>
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
@@ -148,63 +149,67 @@
   </div>
 </header>
 
-<div class="main-container container">
+<div class="main-container">
+  <div class="container">
+    <?php if(howest_banner_get_image() != false):?>
+      <div class="howest-banner">
+       <img src="<?php print howest_banner_get_image();?>"/>
+      </div>
+    <?php endif;?>
 
-  <?php if(howest_banner_get_image() != false):?>
-    <div class="howest-banner">
-     <img src="<?php print howest_banner_get_image();?>"/>
-    </div>
-  <?php endif;?>
-
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3 sidebar_first" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+    <header role="banner" id="page-header">
+      <?php if (!empty($site_slogan)): ?>
+        <p class="lead"><?php print $site_slogan; ?></p>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print t($title); ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3 sidebar" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
+      <?php print render($page['header']); ?>
+    </header> <!-- /#page-header -->
 
+    <div class="row">
+
+      <?php if (!empty($page['sidebar_first'])): ?>
+        <aside class="col-sm-3 sidebar_first" role="complementary">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
+
+      <section<?php print $content_column_class; ?>>
+        <?php if (!empty($page['highlighted'])): ?>
+          <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+        <?php endif; ?>
+        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if (!empty($title)): ?>
+          <h1 class="page-header"><?php print t($title); ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+        <?php endif; ?>
+        <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+        <?php endif; ?>
+        <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+      </section>
+
+      <?php if (!empty($page['sidebar_second'])): ?>
+        <aside class="col-sm-3 sidebar" role="complementary">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+
+    </div> <!-- /row -->
+  </div> <!-- /container -->
+</div>  <!-- /main-container -->
+
+<footer class="footer">
+  <div class="container">
+    &copy; <a href="http://howest.be" target="_blank" title="Hogeschool Howest">howest.be</a> <?php echo date('Y'); ?>
+    <?php print render($page['footer']); ?>
   </div>
-</div>
-<footer class="footer container">
-  &copy; <a href="http://howest.be" target="_blank">howest.be</a> <?php echo date('Y'); ?>
-  <?php print render($page['footer']); ?>
 </footer>
