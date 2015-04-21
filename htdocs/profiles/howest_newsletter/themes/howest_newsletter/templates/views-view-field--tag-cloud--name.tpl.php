d2af96020a2b82282516134d878018e87d4ba355
Wouter Haesaerts <wouter@inuits.eu>
2015-04-21 16:14:26 +0200

    updated theming and added templates.

--- htdocs/profiles/howest_newsletter/themes/howest_newsletter/templates/views-view-field--tag-cloud--name.tpl.php
+++ htdocs/profiles/howest_newsletter/themes/howest_newsletter/templates/views-view-field--tag-cloud--name.tpl.php
@@ -1,3 +1,4 @@
-<?php 
-echo l(t($row->taxonomy_term_data_name), 'taxonomy/term/'.$row->tid);
+<?php
+  // Output the taxonomy term and link to its page.
+  echo l(t($row->taxonomy_term_data_name), 'taxonomy/term/'.$row->tid);
 ?>
--- htdocs/profiles/howest_newsletter/themes/howest_newsletter/templates/views-view-field--tag-cloud.tpl.php
+++ htdocs/profiles/howest_newsletter/themes/howest_newsletter/templates/views-view-field--tag-cloud.tpl.php
@@ -1,1 +1,4 @@
-<?php print t($output); ?>
+<?php
+  // All this does, is output the number of nodes this term is applicable to.
+  print t($output);
+?>
