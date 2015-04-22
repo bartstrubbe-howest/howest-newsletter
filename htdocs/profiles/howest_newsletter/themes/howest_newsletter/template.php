16b96866cd1f6147af8b0d796a05aa85f6e93e12
Wouter Haesaerts <wouter@inuits.eu>
2015-04-22 13:30:47 +0200

    changed search field placeholder, rewrote search URL.

--- htdocs/profiles/howest_newsletter/themes/howest_newsletter/template.php
+++ htdocs/profiles/howest_newsletter/themes/howest_newsletter/template.php
@@ -66,3 +66,9 @@ function howest_newsletter_preprocess_node(&$variables){
   $variables['date'] = format_date($variables['node']->created, 'blog_posted');
   $variables['submitted'] = t('Submitted on: !datetime', array('!username' => $variables['name'], '!datetime' => $variables['date']));
 }
+
+function howest_newsletter_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#attributes']['placeholder'] = t('I\'m looking for...');
+  }
+}
