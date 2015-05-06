6d1db48cae67b4c10c0995ecf2e3843ddf10f390
Jan Laureys <janl@hakka.eu>
2015-05-06 14:52:05 +0200

    Downgrade less module

--- htdocs/sites/all/modules/contrib/less/less.watch.js
+++ htdocs/sites/all/modules/contrib/less/less.watch.js
@@ -1,11 +1,6 @@
-/**
- * @file
- *   Contains JS required for less watch functionality.
- */
 ;(function ($) {
-  "use strict";
-  
-  Drupal.behaviors.less_watch = {
+
+  Drupal.behaviors.less = {
     attach: function (context, settings) {
       
       var watched_files = [],
@@ -14,7 +9,7 @@
       $watched_links = $('head link[src$=".less"]', context).each(function () {
         
         // Only grab the portion of the url up to, but not including, the '?'.
-        watched_files.push($(this).attr('href'));
+        watched_files.push($(this).attr('href').match(/^([^\?]+)/)[1]);
       });
       
       if (watched_files.length > 0) {
@@ -45,21 +40,23 @@
               data: {
                 less_files: watched_files
               },
-
+              timeout: self.interval,
+              
               // On success, reset failed request counter and update style links.
               success: function ( response ) {
                 
                 self.failed_requests = 0;
                 
-                $.each(response, function (index, value) {
-                  var old_file = value.old_file,
-                      new_file = value.new_file;
+                for (i in response) {
+                  
+                  var old_file = response[i].old_file,
+                      new_file = response[i].new_file;
                   
                   // Math.random() at the end forces a reload of the file.
                   $('head link[href^="' + old_file + '"]', context).replaceWith($('<link type="text/css" rel="stylesheet" media="all" />').attr('href', new_file + '?' + Math.random()));
                   
-                  watched_files[$.inArray(old_file, watched_files)] = new_file;
-                });
+                  watched_files[watched_files.indexOf(old_file)] = new_file;
+                }
               },
               
               // On failure, count failed request and increase interval.
